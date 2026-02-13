2354e53f27086c83b57d08fcaf899a0597ec88ae
Beben Sutara <58406018+bensu89@users.noreply.github.com>
2026-02-13 20:21:08 +0700

    Adjust font size in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -132,10 +132,10 @@
                 </div>
 
                 <div class="text-xs text-gray-400 text-center mt-4">
-             <p style="font-size: 16px; font-weight: bold; text-align: center;">
-    Kerjasama Antara Tim KKN dan <br>
-    Pemerintahan Desa Cikurubuk
-</p>
+             <p style="font-size: 14px; font-weight: bold; text-align: center;">
+                Kerjasama Antara Tim KKN dan <br>
+                Pemerintahan Desa Cikurubuk
+                </p>
                 </div>
             </div>
         </div>
